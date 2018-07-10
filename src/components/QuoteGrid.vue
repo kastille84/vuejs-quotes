5a3a8008e9913ca64a86fa5dab93fffc4d94aa78
Edwin Martinez <kastille84@gmail.com>
2018-07-09 20:56:27 -0400

    initial commit

---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="row">
+        <app-quote v-for="quote in quotes" :key="quote"></app-quote>
+    </div>
+</template>
+
+<script>
+    import Quote from './Quote.vue';
+
+    export default {
+        props: ['quotes'],
+        data: function() {
+            return {
+
+            }
+        },
+        components: {
+            appQuote: Quote
+        }
+    }
+</script>
+
+<style>
+
+</style>
+
